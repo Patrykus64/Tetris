8a071fd1c7629ca88ac12e5202fa0758b1a4cb32
Patryk Golab <patrykus64@gmail.com>
2022-08-05 19:31:14 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+const grid = document.querySelector('.grid');
+let squares = Array.from(document.querySelectorAll('.grid div'));
+const scoreDisplay = document.querySelector('#score');
+const startButton = document.querySelector("#start_button");
+
+
+console.log(squares);
+
+
+})
